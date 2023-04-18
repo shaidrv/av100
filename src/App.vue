1ee9acf5a33b9b586c44c1f9ee81f552625e1d59
Rita <shaidrv@gmail.com>
2023-04-18 13:37:49 +0300

    Верстка блока учетная запись

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,47 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="container">
+      <div class="account">
+        <div class="account__title">Учётная запись</div>
+        <div class="account__fields">
+          <div class="row">
+            <label>Компания</label>
+            <div>
+              <input type="text" name="company" />
+            </div>
+          </div>
+          <div class="row">
+            <label>Логин</label>
+            <div>
+              <input type="text" name="login" />
+            </div>
+          </div>
+          <div class="row">
+            <label
+              >Номер <br />
+              телефона</label
+            >
+            <div>
+              <input type="text" name="phone-number" />
+            </div>
+          </div>
+          <div class="row">
+            <label>Имя</label>
+            <div>
+              <input type="text" name="name" />
+            </div>
+          </div>
+          <div class="row">
+            <label>Фамилия</label>
+            <div>
+              <input type="text" name="last-name" />
+              <small>* Не обязательно</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
