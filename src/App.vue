7b0968f9afa3773a1838f0e82aa99cc15b76c299
Rita <shaidrv@gmail.com>
2023-04-19 15:05:11 +0300

    Верстка блока оповещений и кнопки сохранить

--- src/App.vue
+++ src/App.vue
@@ -2,41 +2,85 @@
   <div id="app">
     <div class="container">
       <div class="account">
-        <div class="account__title">Учётная запись</div>
-        <div class="account__fields">
-          <div class="row">
-            <label>Компания</label>
-            <div>
-              <input type="text" name="company" />
+        <div class="account__title title">Учётная запись</div>
+        <div class="account__content">
+          <div class="account__fields">
+            <div class="row">
+              <label>Компания</label>
+              <div>
+                <input type="text" name="company" class="input-field" />
+              </div>
+            </div>
+            <div class="row">
+              <label>Логин</label>
+              <div>
+                <input type="text" name="login" class="input-field" />
+              </div>
+            </div>
+            <div class="row">
+              <label
+                >Номер <br />
+                телефона</label
+              >
+              <div>
+                <input type="text" name="phone-number" class="input-field" />
+              </div>
+            </div>
+            <div class="row">
+              <label>Имя</label>
+              <div>
+                <input type="text" name="name" class="input-field" />
+              </div>
+            </div>
+            <div class="row">
+              <label>Фамилия</label>
+              <div>
+                <input type="text" name="last-name" class="input-field" />
+                <small>* Не обязательно</small>
+              </div>
             </div>
           </div>
-          <div class="row">
-            <label>Логин</label>
-            <div>
-              <input type="text" name="login" />
+        </div>
+      </div>
+      <div class="notification-settings">
+        <div class="notification-settings__title title">
+          Оповещения о новых подборках
+        </div>
+        <div class="notification-settings__content">
+          <p class="notification-settings__description">
+            Выберите, куда будут приходить уведомления при появлении
+            автомобилей, которые подходят под критерии вашей подборки.
+          </p>
+          <div class="notification-settings__fields">
+            <div class="notification-settings__caption">Уведомления</div>
+            <div class="notification-settings__field">
+              <div class="notification-settings__radio">
+                <input
+                  type="radio"
+                  checked
+                  name="notification"
+                  class="input-radio"
+                />
+                <label class="label-radio">Выкл</label>
+              </div>
+            </div>
+            <div class="notification-settings__field">
+              <div class="notification-settings__radio">
+                <input type="radio" name="notification" class="input-radio" />
+                <label class="label-radio">Email</label>
+              </div>
+              <div class="notification-settings__input">
+                <input type="text" class="input-field" />
+              </div>
             </div>
           </div>
-          <div class="row">
-            <label
-              >Номер <br />
-              телефона</label
-            >
-            <div>
-              <input type="text" name="phone-number" />
-            </div>
-          </div>
-          <div class="row">
-            <label>Имя</label>
-            <div>
-              <input type="text" name="name" />
-            </div>
-          </div>
-          <div class="row">
-            <label>Фамилия</label>
-            <div>
-              <input type="text" name="last-name" />
-              <small>* Не обязательно</small>
-            </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <div class="bottom__heading"></div>
+        <div class="bottom__body">
+          <div class="bottom__button">
+            <button>Сохранить</button>
           </div>
         </div>
       </div>
@@ -54,4 +98,8 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+button {
+  font-size: 13px;
+}
+</style>
